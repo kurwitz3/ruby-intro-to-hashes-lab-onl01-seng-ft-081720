7936a072548d2dc17dec22edb683f763d1bc0ace
Richard Kurwitz <kurwitz3@aol.com>
2020-08-21 19:01:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,10 +9,8 @@ end
 
 def monopoly
 	monopoly = { 
-	  :railroads => {}, 
-	            pieces: "4", 
-	            :names => {}, 
-	            :rent_in_dollars => {}
+	  :railroads => {} 
+	           
 	
 	  
 	}
